0269fe954c30bddc575505cc241d1bdae796d9a8
Muhammad Zain Ul Abideen <xenbaloch@live.com>
2020-03-15 13:13:15 +0800

    Update and rename adaline.ipynb to adaline.py

--- adaline.py
+++ adaline.py
@@ -1,6 +1,6 @@
-# Implementing Adaline in Python
-
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class AdalineGD():
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
@@ -51,3 +51,44 @@ class AdalineGD():
         return np.where(self.net_input(X) >= 0.0, 1, -1)
         """Returns a prediction of -1 or 1 depending on a positive or negative 
             dot product"""
+        
+v1 = np.array([1, 2, 3])
+v2 = 0.5 * v1
+np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+        'machine-learning-databases/iris/iris.data', header=None)
+df.tail()
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.show()
